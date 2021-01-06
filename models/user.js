2573fa8f0b50985c14e40a8e075537dce6c6839c
elacays <61878338+elacays@users.noreply.github.com>
2021-01-06 15:39:46 +0300

    User şemasına tarih eklendi

--- models/user.js
+++ models/user.js
@@ -4,9 +4,10 @@ var Schema = mongoose.Schema;
 
 //kullanıcının Şeması
 var userSchema = new Schema({
-    name: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    mail: { type: String, required: true, unique: true }
+    name: { type: String, required: true, unique: true, min: 3, max: 255 },
+    password: { type: String, required: true, min: 6, max: 1024 },
+    mail: { type: String, required: true, unique: true, min: 6, max: 255 },
+    date: { type: Date, default: Date.now }
 
 });
 
